82068d9a2ee616be496e0cfcbcb6316684a506c4
Brian Liu Xu <blx@alumni.cmu.edu>
2015-12-06 06:10:17 -0500

    azure compatibility

--- app.js
+++ app.js
@@ -156,6 +156,6 @@ function ensureAuthenticated(req, res, next) {
 // error-handler settings
 require('./config/error-handler')(app);
 
-var port = process.env.VCAP_APP_PORT || 3000;
+var port = process.env.port || 3000;
 app.listen(port);
 console.log('listening at:', port);
